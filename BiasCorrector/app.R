880495cf93a663945d7bb1f71790d8e66e5d4f43
kapsner <lorenz.kapsner@web.de>
2019-01-22 18:44:24 +0100

    todo massages

--- BiasCorrector/app.R
+++ BiasCorrector/app.R
@@ -12,3 +12,5 @@ shinyAppDir("app")
 
 # TODOs:
 # TODO write file requirements FAQ (transform all constraints within this algorithm to human readable format)
+# TODO link readme and FAQ to github pages
+# TODO bundle all files/ plots for downloading a zip-file on last page
